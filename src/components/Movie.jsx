f536ada4ab44881484b7aba9e4b930b43cc6824f
Yash <yashtrivedi2000@outlook.com>
2021-04-05 00:54:57 +0530

    delete functionality added!

---
+++ src/components/Movie.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { getMovies } from "../Starter Code/services/fakeMovieService";
+class Movie extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDeleteMovie = (mov) => {
+    const movies = this.state.movies.filter((movie) => movie._id !== mov._id);
+    console.log(movies);
+    this.setState({ movies });
+  };
+  render() {
+    if (this.state.movies.length === 0)
+      return <p>There is no movies in the database at this time!</p>;
+    return (
+      <div>
+        <p>There are {this.state.movies.length} movies on the list</p>
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDeleteMovie(movie)}
+                    className="btn btn-danger"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  //Methods
+}
+
+export default Movie;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Movie from "./components/Movie";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Movie />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
