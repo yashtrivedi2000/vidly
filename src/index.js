35b024775641d4a7dfbf665fb7cba7cde138913d
Yash <yashtrivedi2000@outlook.com>
2021-04-08 02:06:20 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Movie from "./components/Movie";
+import Movie from "./components/movie";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
